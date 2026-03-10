fe6eea7a00e21e20de4b823c6a5e1f2bf337edba
ylan58825-spec <ylan58825@gmail.com>
2026-03-10 21:25:00 +0800

    20260310 git push -u origin main
    git commit -m 20260310

---
+++ GUl_Class N13310030/GUl_Class N13310030/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUl_Class_N13310030
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
